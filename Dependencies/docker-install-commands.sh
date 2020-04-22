5acaf7a1c4547b3e22d044a37192886c959912e5
root <root@IL1ZZQFANCHR01.local>
2020-04-22 03:46:08 +0000

    Revised Docker install commands

--- Dependencies/docker-install-commands.sh
+++ Dependencies/docker-install-commands.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+sudo apt-get update -y
+sudo apt-get install -y software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 docker info
 sudo systemctl status docker
